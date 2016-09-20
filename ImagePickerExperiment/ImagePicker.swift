48ecbb7ea3d1b0cce626e53430a6be319f45dc5e
Jodie Alaine Parker <me@jodiealaine.com>
2016-09-20 02:00:18 +0100

    As a user I can pick an image from my photo library and display it

---
+++ ImagePickerExperiment/ImagePicker.swift
@@ -0,0 +1,19 @@
+//
+//  imagePicker.swift
+//  ImagePickerExperiment
+//
+//  Created by JODIE PARKER on 19/09/2016.
+//  Copyright © 2016 urbanshed. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImagePickerDelegate: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+}
--- ImagePickerExperiment/ViewController.swift
+++ ImagePickerExperiment/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: UIImagePickerControllerDelegate
+    
+    // When I cancel then I want to return to the original scene
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // When I select an image then I want that image to be displayed in UIImageView
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imagePickerView.image = selectedImage
+        dismiss(animated: true, completion: nil)
     }
 
-
+    // MARK: Actions
+    
+    @IBAction func selectImage(_ sender: UIBarButtonItem) {
+        // When I click on Pick I want the UIImagePickerController to show my images
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
 }
 
